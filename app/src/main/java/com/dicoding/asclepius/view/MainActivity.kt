876f4e813b67a6c6a35d63b3e1beb56516387a7a
AdiSastrawan <adisastrawan110703@gmail.com>
2024-03-25 16:56:21 +0800

    trying ucrop

--- app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
+++ app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
@@ -2,19 +2,31 @@ package com.dicoding.asclepius.view
 
 import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import com.dicoding.asclepius.R
 import com.dicoding.asclepius.databinding.ActivityMainBinding
 import com.dicoding.asclepius.helper.ImageClassifierHelper
+import com.yalantis.ucrop.UCrop
+import com.yalantis.ucrop.util.FileUtils
 import org.tensorflow.lite.task.vision.classifier.Classifications
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
 import java.text.NumberFormat
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -27,6 +39,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        if(checkStoragePermissions()){
+            showToast("Permission Granted")
+        }else{
+            showToast("Permission Denied")
+        }
         binding.galleryButton.setOnClickListener{
             startGallery()
         }
@@ -39,14 +56,26 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
-
+    fun checkStoragePermissions(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            //Android is 11 (R) or above
+            Environment.isExternalStorageManager()
+        } else {
+            //Below android 11
+            val write =
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            val read =
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            read == PackageManager.PERMISSION_GRANTED && write == PackageManager.PERMISSION_GRANTED
+        }
+    }
     private fun startGallery() {
         launchGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
     }
 
     private fun showImage() {
         currentImageUri?.let {
-            binding.previewImageView.setImageURI(currentImageUri)
+            binding.previewImageView.setImageURI(it)
         }
     }
 
@@ -84,7 +113,6 @@ class MainActivity : AppCompatActivity() {
         imageClassifierHelper?.classifyStaticImage(currentImageUri!!)
 
     }
-
     private fun moveToResult(result : String) {
         val intent = Intent(this, ResultActivity::class.java)
         intent.putExtra(ResultActivity.EXTRA_IMAGE,currentImageUri.toString())
@@ -100,11 +128,34 @@ class MainActivity : AppCompatActivity() {
         ActivityResultContracts.PickVisualMedia()
     ){ uri ->
         if(uri != null){
-            currentImageUri = uri
+            val storageDir = File(
+                Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_PICTURES
+                ), "Test"
+            )
+            val destinationUri = File(storageDir,"${System.currentTimeMillis()}.jpg").toUri()
+            Log.d("destinationUri",destinationUri.toString())
+            UCrop.of(uri, destinationUri)
+                .withAspectRatio(1f, 1f)
+                .withMaxResultSize(2000, 2000)
+                .start(this);
             showImage()
         }else{
             Log.d("Photo Picker", "No media selected")
         }
 
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+            val resultUri = UCrop.getOutput(data!!)
+            showToast(resultUri.toString())
+            currentImageUri = resultUri
+            Log.d("ImageUri",currentImageUri.toString())
+        } else if (resultCode == UCrop.RESULT_ERROR) {
+            val cropError = UCrop.getError(data!!)
+            Log.e("CropError",cropError.toString())
+            showToast(cropError?.message.toString())
+        }
     }
 }
