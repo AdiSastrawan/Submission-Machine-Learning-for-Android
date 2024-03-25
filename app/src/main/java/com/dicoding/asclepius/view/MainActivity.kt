778e708069c7ef7456776d31e01e3ffb72e33add
AdiSastrawan <adisastrawan110703@gmail.com>
2024-03-25 18:26:51 +0800

    feat:adding crop

--- app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
+++ app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
@@ -2,8 +2,8 @@ package com.dicoding.asclepius.view
 
 import android.Manifest
 import android.content.Intent
+import android.content.Intent.ACTION_GET_CONTENT
 import android.content.pm.PackageManager
-import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -15,16 +15,15 @@ import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.core.net.toUri
 import com.dicoding.asclepius.R
 import com.dicoding.asclepius.databinding.ActivityMainBinding
 import com.dicoding.asclepius.helper.ImageClassifierHelper
 import com.yalantis.ucrop.UCrop
-import com.yalantis.ucrop.util.FileUtils
 import org.tensorflow.lite.task.vision.classifier.Classifications
 import java.io.File
-import java.io.FileInputStream
-import java.io.InputStream
+import java.io.IOException
 import java.text.NumberFormat
 
 
@@ -39,11 +38,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        if(checkStoragePermissions()){
-            showToast("Permission Granted")
-        }else{
-            showToast("Permission Denied")
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
         }
+
         binding.galleryButton.setOnClickListener{
             startGallery()
         }
@@ -56,19 +54,7 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
-    fun checkStoragePermissions(): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            //Android is 11 (R) or above
-            Environment.isExternalStorageManager()
-        } else {
-            //Below android 11
-            val write =
-                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            val read =
-                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-            read == PackageManager.PERMISSION_GRANTED && write == PackageManager.PERMISSION_GRANTED
-        }
-    }
+
     private fun startGallery() {
         launchGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
     }
@@ -124,22 +110,16 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
+
+
     private val launchGallery = registerForActivityResult(
         ActivityResultContracts.PickVisualMedia()
     ){ uri ->
         if(uri != null){
-            val storageDir = File(
-                Environment.getExternalStoragePublicDirectory(
-                    Environment.DIRECTORY_PICTURES
-                ), "Test"
-            )
-            val destinationUri = File(storageDir,"${System.currentTimeMillis()}.jpg").toUri()
-            Log.d("destinationUri",destinationUri.toString())
-            UCrop.of(uri, destinationUri)
+            UCrop.of(uri, uri)
                 .withAspectRatio(1f, 1f)
                 .withMaxResultSize(2000, 2000)
                 .start(this);
-            showImage()
         }else{
             Log.d("Photo Picker", "No media selected")
         }
@@ -149,9 +129,8 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
             val resultUri = UCrop.getOutput(data!!)
-            showToast(resultUri.toString())
             currentImageUri = resultUri
-            Log.d("ImageUri",currentImageUri.toString())
+            showImage()
         } else if (resultCode == UCrop.RESULT_ERROR) {
             val cropError = UCrop.getError(data!!)
             Log.e("CropError",cropError.toString())
